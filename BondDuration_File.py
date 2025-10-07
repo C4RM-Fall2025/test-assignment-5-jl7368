9a647d60ae8ef13632a0955e8231c0312d13b109
jl7368 <jl7368@columbia.edu>
2025-10-06 20:33:17 -0400

    Add files via upload

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -3,14 +3,16 @@ import numpy as np
 def getBondDuration(y, face, couponRate, m, ppy = 1):
     t = np.linspace(1/ppy, m, m * ppy)
 
-    pv = 1 / (1 + y / ppy) ** t
+    pv = 1 / (1 + y / ppy) ** (t*ppy)
+    print(pv)
 
     cf = couponRate * face / ppy * np.ones(m * ppy - 1)
     cf = np.append(cf, couponRate * face / ppy + face)
 
-    weighted_pv = t * pv
+    weighted_pv = sum(t * pv * cf)
 
-    price = pv @ cf.transpose()
-    dur = (weighted_pv @ cf).transpose() / price
+    pvcf = sum(pv * cf)
+    dur = weighted_pv / pvcf
 
     return dur
+print(getBondDuration(0.03, 2000000, 0.04, 10, 2))
